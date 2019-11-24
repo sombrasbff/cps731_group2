2b40de4e754547447e32ebf18cf3fd23bb663107
Hydrolios <38369814+Hydrolios@users.noreply.github.com>
2019-11-24 17:35:26 -0500

    Create launch.java
    Launch of the application starts here

---
+++ launch.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class launch
+{
+	public static void main(String[] args)
+	   {  
+	      JFrame frame = new loginscreen();	  
+	      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      frame.setTitle("Reach");
+	      frame.setVisible(true);      
+	   }
+}
