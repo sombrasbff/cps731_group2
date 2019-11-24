f3328c272e547738479aa6bc0b945d53cf7c148d
Hydrolios <38369814+Hydrolios@users.noreply.github.com>
2019-11-24 17:17:26 -0500

    Update book.java
    Added a few lines to implement the feature of adding a new book

--- book.java
+++ book.java
@@ -7,6 +7,59 @@ public class book {
 	public double price;
 	public int rating;
 	
+	public book (String title, String author, String ISBN, String genre, double price, int rating)
+	{
+		this.title = title;
+		this.author = author;
+		this.ISBN = ISBN;
+		this.genre = genre;
+		this.price = price;
+		this.rating = rating;
+	}
+	public String getTitle()
+	{
+		return title;
+	}
+	public void setTitle(String newTitle)
+	{
+		this.title = newTitle;
+	}
+	public String getAuthor()
+	{
+		return author;
+	}
+	public void setAuthor(String newAuthor)
+	{
+		this.author = newAuthor;
+	}
+	public String getISBN()
+	{
+		return ISBN;
+	}
+	public void setISBN(String newISBN)
+	{
+		this.ISBN = newISBN;
+	}
+	public String getGenre()
+	{
+		return genre;
+	}
+	public void setGenre(String newGenre)
+	{
+		this.genre = newGenre;
+	}
+	public double getPrice()
+	{
+		return price;
+	}
+	public void setPrice(String newPrice)
+	{
+		this.price = newPrice;
+	}
+	public int getRating()
+	{
+		return rating;
+	}
 	public void search() {
 		
 	}
