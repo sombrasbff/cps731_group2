8530caaf9b1e5f5e6552a1e36ca7752ecb0791be
sombrasbff <incubatorsincorperated@gmail.com>
2019-11-24 12:56:30 -0500

    Implementing reader functionality Issue #2
    Issue #2

--- reader.java
+++ reader.java
@@ -1,9 +1,26 @@
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
 
 public class reader {
 	public String title;
 	public String author;
 	public String text;
 	public int pageNumber;
+	public static void main(String[] args) {
+		open();
+	}
+	public static void open() {
+		if (Desktop.isDesktopSupported()) {
+		    try {
+			/* Change file path to run */    
+		        File myFile = new File("Path/To/Book.pdf");
+		        Desktop.getDesktop().open(myFile);
+		    } catch (IOException ex) {
+		        // no application registered for PDFs
+		    }
+		}
+	}
 	public void nextPage() {
 		
 	}
